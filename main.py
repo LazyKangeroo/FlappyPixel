7006c672ca14a57d395c048b1f1242d0e4041555
Fox1508 <fvantonder00@gmail.com>
2022-12-14 15:10:00 +0200

    Scrapping all the old code
    Because There are alot of unfixable bugs. And starting a new.  And testing the code every module at a time

--- main.py
+++ main.py
@@ -1,70 +1,43 @@
-from shutil import move
+# Moverment
 from microbit import *
-import random
 
-class Flappy:
-    Non = 0
-    Up = 1
-    Down = 2
+class Move:
+    Up = 0
+    Down = 1
 
-    PLAYERBRIGHT = 9
-    PILLERSBRIGHT = 6
-    SAMPLETIME = 50
-    SAMPELERMOVE = 10
+    y = 1
+    x = 1
+    brightness = 9
+
+
+    display.clear()
 
     def __init__(self):
         pass
 
-    def startGame(self):
-        display.clear()
-        self.dirction = self.Non
-        self.score = 0
-
-        playing = True
-
-        sample = 0
-        while (playing):
-            sleep(self.SAMPLETIME)
-            ButtonPressed = self.handelBtn()
-            sample = sample +1
-
-            if ButtonPressed or sample == self.SAMPELERMOVE:
-                playing = self.move()
-                sample = 0
-
-        display.scroll("Score = " + str(self.score), 100)
-        display.clear()
+    def Go(self):
+        #display.set_pixel(self.x,self.y,self.brightness)
+        while True:
+            self.direction = self.handelBtn()
+            sleep(1000)
+            display.clear()
+            self.ChangePostion()
+            display.set_pixel(self.x,self.y,self.brightness)
 
     def handelBtn(self):
-        ButtonPressed = False
-
+        while not button_a.is_pressed() and not button_b.is_pressed():
+            display.set_pixel(self.x,self.y,self.brightness)
         if button_a.is_pressed():
-            while (button_a.is_pressed()):
-                sleep(self.SAMPLETIME)
-            self.down()
-            ButtonPressed = True
+            return self.Up
+        if button_b.is_pressed():
+            return self.Down
 
-        elif button_b.is_pressed():
-            while (button_b.is_pressed()):
-                sleep(self.SAMPLETIME)
-            self.up()
-            ButtonPressed = True
+    def ChangePostion(self):
+        if self.direction == self.Up:
+            self.y = self.y - 1
+        if self.direction == self.Down:
+            self.y = self.y + 1
+        return
 
-        return ButtonPressed
-
-    def down(self):
-        self.dirction = self.Down
-        move()
-
-    def up(self):
-        self.dirction = self.Up
-        move()
-
-    def move(self):
-        x = 1
-        if self.dirction == self.Up:
-            y = y -1
-        if self.dirction == self.Down:
-            y = y + 1
-
-        display(x,y,self.PLAYERBRIGHT)
+move = Move()
+move.Go()
