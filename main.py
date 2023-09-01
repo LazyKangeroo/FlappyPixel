2c4d9946c41b13e56df2b9187f4e21a89f464c96
Fox1508 <fvantonder00@gmail.com>
2023-09-01 18:16:44 +0200

    Update Code

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
-# Moverment
 from microbit import *
+import random
 
-class Move:
-    Up = 0
-    Down = 1
+class Play:
+    MOVETIME = 400
 
+    StartX = 4
+    EndX = 0
+
+    PLAYER = 9
+    PILLAR = 3
+
+    UP = 0
+    DOWN = 1
     y = 1
     x = 1
-    brightness = 9
-
-    display.clear()
 
     def __init__(self):
         pass
 
     def Go(self):
-        #display.set_pixel(self.x,self.y,self.brightness)
-        while True:
-            self.direction = self.handelBtn()
-            sleep(1000)
+        self.list = [1,2,3]
+        self.pillarX = self.StartX
+        playing = True
+
+        while playing:
+            playing = self.colition()
+            self.handelBtn()
+            self.creatPillar()
+        if playing == False:
             display.clear()
-            self.ChangePostion()
-            display.set_pixel(self.x,self.y,self.brightness)
+            display.scroll(f"Score : {self.sample}")
 
     def handelBtn(self):
-        while not button_a.is_pressed() and not button_b.is_pressed():
-            display.set_pixel(self.x,self.y,self.brightness)
+        d = {}
         if button_a.is_pressed():
-            return self.Up
+            d =  self.UP
         if button_b.is_pressed():
-            return self.Down
+            d = self.DOWN
 
-    def ChangePostion(self):
-        if self.direction == self.Up:
+        if d == self.UP:
             self.y = self.y - 1
-        if self.direction == self.Down:
+        if d == self.DOWN:
             self.y = self.y + 1
 
         if self.y < 0:
@@ -43,5 +49,41 @@ class Move:
             self.y = 0
         return
 
-move = Move()
-move.Go()
+    def displayPillars(self):
+        display.clear()
+
+        for y in range(5):
+            if y == self.pillarSafeY: continue
+            display.set_pixel(self.pillarX,y,self.PILLAR)
+            self.player()
+        sleep(self.MOVETIME)
+
+    def player(self):
+        display.set_pixel(self.x,self.y,self.PLAYER)
+
+
+    def creatPillar(self):
+        if self.pillarX == self.StartX:
+            self.pillarSafeY = random.choice(self.list)
+
+        if self.pillarX == self.EndX:
+            self.pillarX = self.StartX
+            self.pillarSafeY = random.choice(self.list)
+
+        self.displayPillars()
+
+        if self.pillarX != self.EndX:
+            self.pillarX = self.pillarX - 1
+        return
+
+    def colition(self):
+        if self.pillarX == self.x and self.pillarSafeY != self.y:
+            return False
+        elif self.pillarX == self.x and self.pillarSafeY == self.y:
+            self.sample = self.sample + 1
+            return True
+        else:
+            return True
+
+p = Play()
+p.Go()
